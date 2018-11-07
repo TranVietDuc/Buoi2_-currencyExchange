bea1db089b7bae060d21921b03d247eaca86475e
TranVietDuc <44611445+TranVietDuc@users.noreply.github.com>
2018-11-07 15:46:47 +0700

    first commit

---
+++ code.js
@@ -0,0 +1,39 @@
+function Inform(){
+   var select1;
+
+}
+
+
+
+
+
+
+
+/*var amount;
+var select1;
+var select2;
+var result;
+function GetAmount() {
+    amount = parseInt(document.getElementById("amount").value);
+}
+function Exchange() {
+    select1 = document.getElementById("select1").value;
+    select2 = document.getElementById("select2").value;
+    document.getElementById("result").innerText = select1;
+}
+ /*
+ if (select1===USD && select2===VND){
+     result = 23000;
+     alert(result);
+
+ }
+ else{}
+}
+ /*else {
+     if (select1===VND && select2===USD){
+         result = amount /23000;
+     }
+     else {}
+ }
+ document.getElementById("result").innerText= result;
+}
